6b48696dfbb441522ed448195cd993309ef79d22
Richard Basdeo <richard@nextiles.tech>
2022-05-17 11:25:16 -0400

    Initial project configuration. - Richard

---
+++ nextiles-demo/nextiles-demo/View/DemoView.swift
@@ -0,0 +1,20 @@
+//
+//  DemoView.swift
+//  nextiles-demo
+//
+//  Created by Work on 5/17/22.
+//
+
+import SwiftUI
+
+struct DemoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DemoView_Previews: PreviewProvider {
+    static var previews: some View {
+        DemoView()
+    }
+}
---
+++ nextiles-demo/nextiles-demo/nextiles_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  nextiles_demoApp.swift
+//  nextiles-demo
+//
+//  Created by Work on 5/17/22.
+//
+
+import SwiftUI
+
+@main
+struct nextiles_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DemoView()
+        }
+    }
+}
